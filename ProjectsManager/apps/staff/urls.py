71c70409b9e28a8f9da122eb89ad40cbb7dc2cfd
tund3rs <ngductu.kma@gmail.com>
2020-03-02 14:57:42 +0700

    init project structure

---
+++ ProjectsManager/apps/staff/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from apps.staff import views
+router = DefaultRouter()
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')),
+]
