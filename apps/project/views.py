9ab68dc56577e859ab20ae95fc93f0ca7bee6a01
Tu NguyenDuc <ngductu.kma@gmail.com>
2020-03-15 20:45:52 +0700

    create and update effort project

--- apps/project/views.py
+++ apps/project/views.py
@@ -1,28 +1,49 @@
-from django.db.models import Prefetch, Sum, Q
+import csv
+
+from django.db.models import Prefetch, Sum, Q, Count
 from datetime import datetime
 
-from django.db.models.functions import ExtractMonth, ExtractYear
+from django.db.models.functions import ExtractMonth, ExtractYear, Concat
+from django.http import HttpResponse
 from django.utils.decorators import method_decorator
 from drf_yasg.utils import swagger_auto_schema
+from rest_framework.decorators import action
 
 from .serializers import *
 from rest_framework import viewsets
 
-
 # Create your views here.
+from ..utils.views_helper import GenericViewSet
+
+
 @method_decorator(name='update', decorator=swagger_auto_schema(auto_schema=None))
-class ProjectsPlansViewSet(viewsets.ModelViewSet):
-    current_date = datetime.now()
-    queryset = Projects.objects.prefetch_related(
-        Prefetch(
-            "planprojects_set",
-            queryset=PlanProjects.objects.annotate(
-                _actual_effort=Sum("project__staffprojects__effort",
-                                   filter=Q(project__staffprojects__start_date__month=ExtractMonth('start_date')) &
-                                          Q(project__staffprojects__start_date__year=ExtractYear('start_date')))
-            ).filter(start_date__month__lte=current_date.month + 1,
-                     start_date__month__gte=current_date.month - 1),
-            to_attr="_month_project"
+class ProjectsPlansViewSet(GenericViewSet):
+    queryset = Projects.objects.all()
+    serializer_class = ProjectCreateRequestSerializer
+    action_serializers = {
+        "create_request": ProjectCreateRequestSerializer,
+        "list_response": ProjectsPlansSerializer,
+        "create_response": ProjectCreateResponseSerializer,
+    }
+
+    def list(self, request, *args, **kwargs):
+        current_date = datetime.datetime.now()
+        self.queryset = Projects.objects.prefetch_related(
+            Prefetch(
+                "planprojects_set",
+                queryset=PlanProjects.objects.annotate(
+                    _actual_effort=Sum("project__staffprojects__effort",
+                                       filter=Q(project__staffprojects__start_date__month=ExtractMonth('start_date')) &
+                                              Q(project__staffprojects__start_date__year=ExtractYear('start_date')))
+                ).filter(start_date__month__lte=current_date.month + 1,
+                         start_date__month__gte=current_date.month - 1),
+                to_attr="_month_project"
+            )
         )
-    )
-    serializer_class = ProjectsPlansSerializer
+        return super().list(request, *args, **kwargs)
+
+    def update(self, request):
+        pass
+
+    # def destroy(self, request):
+    #     pass
--- apps/utils/views_helper.py
+++ apps/utils/views_helper.py
@@ -7,6 +7,74 @@ from rest_framework.response import Response
 import apps.utils.response_interface as rsp
 
 
+# from apps.utils.base.permissions import IsAdminUserOrReadOnly, ReadOnly
+
+
+class GenericViewSet1(mixins.CreateModelMixin,
+                      mixins.ListModelMixin,
+                      mixins.RetrieveModelMixin,
+                      mixins.UpdateModelMixin,
+                      mixins.DestroyModelMixin,
+                      viewsets.GenericViewSet):
+    # permission_classes = (IsAdminUserOrReadOnly,)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        general_response = rsp.Response(True, serializer.data).generate_response()
+        return Response(general_response, status=status.HTTP_201_CREATED, headers=headers)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            general_response = rsp.Response(True, self.get_paginated_response(serializer.data).data).generate_response()
+            return Response(general_response, status=status.HTTP_200_OK)
+
+        serializer = self.get_serializer(queryset, many=True)
+        general_response = rsp.Response(True, serializer.data).generate_response()
+        return Response(general_response, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        general_response = rsp.Response(True, serializer.data).generate_response()
+        return Response(general_response, status=status.HTTP_200_OK)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        general_response = rsp.Response(True, serializer.data).generate_response()
+        return Response(general_response, status=status.HTTP_200_OK)
+
+    def partial_update(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        return self.update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        general_response = rsp.Response(True, None).generate_response()
+        return Response(general_response, status=status.HTTP_200_OK)
+
+    def custom_response(self, data, message=None, message_param=None):
+        general_response = rsp.Response(True, data).generate_response()
+        return Response(general_response, status=status.HTTP_200_OK)
+
+
 class SerializerMixin(object):
     action_serializers = {}
     request_serializer_class = None
@@ -40,15 +108,15 @@ class QuerySetMixin(object):
 
 
 class GenericViewSet(mixins.CreateModelMixin,
-                        mixins.ListModelMixin,
-                        mixins.RetrieveModelMixin,
-                        mixins.UpdateModelMixin,
-                        mixins.DestroyModelMixin,
-                        viewsets.GenericViewSet,
-                        SerializerMixin,
-                        QuerySetMixin,
-                        ):
-    # permission_classes = (IsAuthenticated,)
+                     mixins.ListModelMixin,
+                     mixins.RetrieveModelMixin,
+                     mixins.UpdateModelMixin,
+                     mixins.DestroyModelMixin,
+                     viewsets.GenericViewSet,
+                     SerializerMixin,
+                     QuerySetMixin,
+                     ):
+    # permission_classes = (IsAuthenticated | ReadOnly,)
 
     def perform_create(self, serializer):
         return serializer.save()
@@ -170,3 +238,4 @@ class GenericViewSet(mixins.CreateModelMixin,
         else:
             response['data'] = data
         return response
+
